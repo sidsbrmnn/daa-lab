a768ad7dddd734dad2cd04b7f74d582090637adc
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-11-06 00:20:40 +0530

    Update MergeSort.java
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -2,57 +2,54 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MergeSort {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		Random r = new Random();
-		System.out.print("Enter no of elements: ");
-		int n = sc.nextInt();
-		int a[] = new int[n];
-		System.out.print("Array has:");
-		for (int i = 0; i < n; i++) {
-			a[i] = r.nextInt(1000);
-			System.out.print(" " + a[i]);
-		}
-		long start = System.nanoTime();
-		sort(a, 0, n - 1);
-		long stop = System.nanoTime();
-		System.out.print("\nSORTED ARRAY: ");
-		for (int i = 0; i < n; i++)
-			System.out.print(" " + a[i]);
-		System.out.println("\nTime taken to sort: " + (stop - start));
-		sc.close();
-	}
+    private static void merge(int[] arr, int low, int high, int mid) {
+        int[] temp = new int[arr.length];
+        int i = low, j = mid + 1, count = low;
 
-	static void sort(int a[], int low, int high) {
-		if (low < high) {
-			int mid = (low + high) / 2;
-			sort(a, low, mid);
-			sort(a, mid + 1, high);
-			merge(a, low, high, mid);
-		}
-	}
+        while (i <= mid && j <= high) {
+            temp[count++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+        }
 
-	static void merge(int a[], int low, int high, int mid) {
-		int temp[] = new int[a.length];
-		int i, j, h;
-		h = i = low;
-		j = mid + 1;
-		while ((h <= mid) && (j <= high)) {
-			if (a[h] < a[j])
-				temp[i++] = a[h++];
-			else
-				temp[i++] = a[j++];
-		}
+        while (i <= mid) {
+            temp[count++] = arr[i++];
+        }
 
-		if (h > mid)
-			for (int k = j; k <= high; k++)
-				temp[i++] = a[k];
-		else
-			for (int k = h; k <= mid; k++)
-				temp[i++] = a[k];
+        while (j <= high) {
+            temp[count++] = arr[j++];
+        }
 
-		for (int k = low; k <= high; k++)
-			a[k] = temp[k];
-	}
+        for (int k = low; k <= high; k++) {
+            arr[k] = temp[k];
+        }
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            sort(arr, low, mid);
+            sort(arr, mid + 1, high);
+            merge(arr, low, high, mid);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter no of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+
+        long startTime = System.nanoTime();
+        sort(arr, 0, n - 1);
+        long stopTime = System.nanoTime();
+
+        System.out.println("Time taken to sort: " + (stopTime - startTime));
+
+        scanner.close();
+    }
 }
