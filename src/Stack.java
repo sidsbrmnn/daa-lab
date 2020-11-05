07db0d0dc021f219a5ab31ac75b5597667840e32
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-11-06 00:20:03 +0530

    Update Stack.java
    - throw Exceptions for stack empty/overflow
    
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- src/Stack.java
+++ src/Stack.java
@@ -1,68 +1,88 @@
 import java.util.Scanner;
 
 public class Stack {
-	Scanner sc = new Scanner(System.in);
-	int max, s[], top;
+    private int size, top;
+    private int[] items;
 
-	Stack(int n) {
-		max = n;
-		s = new int[max];
-		top = -1;
-	}
+    public Stack(int size) {
+        this.top = -1;
+        this.size = size;
+        this.items = new int[size];
+    }
 
-	void push() {
-		if (top == max - 1)
-			System.out.println("Stack overflow.");
-		else {
-			System.out.print("Enter element to push: ");
-			s[++top] = sc.nextInt();
-		}
-	}
+    public void push(int value) throws Exception {
+        if (top == size - 1) {
+            throw new Exception("stack is full");
+        }
 
-	void pop() {
-		if (top == -1)
-			System.out.println("Stack underflow.");
-		else
-			System.out.println(s[top--] + " is popped from the stack.");
-	}
+        items[++top] = value;
+    }
 
-	void display() {
-		if (top == -1)
-			System.out.println("Stack underflow.");
-		else {
-			System.out.println("Stack has:");
-			for (int i = top; i > -1; i--)
-				System.out.println(s[i]);
-		}
-	}
+    public int pop() throws Exception {
+        if (top == -1) {
+            throw new Exception("stack is empty");
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter stack size: ");
-		int n = sc.nextInt();
-		Stack stk = new Stack(n);
-		System.out.println("1. Push 2. Pop 3. Display 4. Exit");
-		while (true) {
-			System.out.print("Enter choice: ");
-			int ch = sc.nextInt();
-			switch (ch) {
-			case 1:
-				stk.push();
-				break;
-			case 2:
-				stk.pop();
-				break;
-			case 3:
-				stk.display();
-				break;
-			case 4:
-				sc.close();
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Invalid choice.");
-			}
-		}
-	}
+        return items[top--];
+    }
+
+    public void display() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println("Stach has:");
+            for (int i = top; i > -1; i--) {
+                System.out.println(items[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter stack size: ");
+        int size = scanner.nextInt();
+        Stack stack = new Stack(size);
+
+        System.out.println("\n1. Push\n2. Pop\n3. Display\n4. Exit");
+        while (true) {
+            System.out.print("\nEnter your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter element: ");
+                    int value = scanner.nextInt();
+                    try {
+                        stack.push(value);
+                        System.out.println(value + " pushed to stack");
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        value = stack.pop();
+                        System.out.println(value + " popped from stack");
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+
+                    break;
+
+                case 3:
+                    stack.display();
+                    break;
+
+                case 4:
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        }
+    }
 }
