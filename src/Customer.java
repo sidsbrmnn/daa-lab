72312c6b784336b06ae87ddd225216a863ccf368
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-11-06 00:17:47 +0530

    Update Customer.java
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- src/Customer.java
+++ src/Customer.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Customer {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter name and DoB in the format <name, DD/MM/YYYY>: ");
-		String name = sc.nextLine();
-		StringTokenizer st = new StringTokenizer(name, "/");
-		int count = st.countTokens();
-		for (int i = 1; i <= count && st.hasMoreTokens(); i++) {
-			System.out.print(st.nextToken());
-			if (i < count)
-				System.out.print(", ");
-		}
-		System.out.println();
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name and date of birth <name, dd/mm/yyyy>: ");
+        String input = scanner.nextLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(input, "/");
+        while (stringTokenizer.hasMoreTokens()) {
+            System.out.print(stringTokenizer.nextToken());
+            if (stringTokenizer.hasMoreTokens()) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        scanner.close();
+    }
 }
