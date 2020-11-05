edef0fdb1e9a9c2da6fc5fa67662795e00912528
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-11-06 00:17:56 +0530

    Update Division.java
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

--- src/Division.java
+++ src/Division.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
 public class Division {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter 2 numbers: ");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		try {
-			int result = a / b;
-			System.out.println("Quotient: " + result);
-		} catch (ArithmeticException e) {
-			System.out.println("EXCEPTION CAUGHT: Enter a non-zero divisor.");
-		}
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter 2 numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        try {
+            int result = a / b;
+            System.out.println("Quotient: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot / by zero");
+        }
+
+        scanner.close();
+    }
 }
